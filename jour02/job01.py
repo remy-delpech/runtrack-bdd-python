41e6fc33a78805c39dd8d7a557a059d68d02b195
Rémy DELPECH <remy.delpech@laplateforme.io>
2023-05-03 10:41:14 +0200

    Création du dossier jour02 et du premier fichier job01

---
+++ jour02/job01.py
@@ -0,0 +1,29 @@
+import mysql.connector
+
+# Remplacez les valeurs ci-dessous par les informations de connexion de votre base de données
+config = {
+  'user': 'root',
+  'password': 'Natalia280988!',
+  'host': 'localhost',
+  'database': 'LaPlateforme',
+  'raise_on_warnings': True
+}
+
+# Se connecter à la base de données
+cnx = mysql.connector.connect(**config)
+
+# Créer un curseur pour exécuter des requêtes SQL
+cursor = cnx.cursor()
+
+# Exécuter la requête SQL pour récupérer tous les étudiants
+query = "SELECT * FROM etudiants;"
+cursor.execute(query)
+
+# Afficher le résultat de la requête en console
+print("Liste des étudiants :")
+for (id, nom, prenom, age, email) in cursor:
+    print(f"{id}: {prenom} {nom} - {age} ans - {email}")
+
+# Fermer le curseur et la connexion
+cursor.close()
+cnx.close()
