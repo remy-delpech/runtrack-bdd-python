be30afa77ffe7aa8da08ac5993523b16cfe0fd82
Rémy DELPECH <remy.delpech@laplateforme.io>
2023-05-03 11:09:46 +0200

    Ajout du job02 et modification du job01 pour ne pas faire apparaitre le vrai mot de passe en ligne

---
+++ jour02/job02.sql
@@ -0,0 +1,14 @@
+CREATE TABLE etage (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nom VARCHAR(255) NOT NULL,
+  numero INT NOT NULL,
+  superficie INT NOT NULL
+);
+
+CREATE TABLE salles (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nom VARCHAR(255) NOT NULL,
+  id_etage INT NOT NULL,
+  capacite INT NOT NULL,
+  FOREIGN KEY (id_etage) REFERENCES etage(id)
+);
